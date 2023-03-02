52f44eb9d94b0fbbd00bbe20391c99da80d07dc8
Exedward <eduardo.ex7@gmail.com>
2023-03-02 15:40:32 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,16 @@ import Profile from './pages/Profile'
 import Favorites from './pages/Favorites'
 import Search from './pages/Search'
 
+import { Ionicons } from '@expo/vector-icons' //No react-native: react-native-vector-icons
+
 const Tab = createBottomTabNavigator()
 
 export default function Routes() {
   return (
     <Tab.Navigator
         screenOptions={{
+            tabBarActiveTintColor: 'red',
+            tabBarInactiveTintColor: 'white',
             tabBarShowLabel: false,
             tabBarStyle:{
                 position: 'absolute',
@@ -24,14 +28,54 @@ export default function Routes() {
                 elevation: 0,
                 borderRadius: 7,
                 height: 30,
-                
+
             }
         }}
     >
-        <Tab.Screen name='Home' component={Home}/>
-        <Tab.Screen name='Profile' component={Profile}/>
-        <Tab.Screen name='Search' component={Search}/>
-        <Tab.Screen name='Favorites' component={Favorites}/>
+        <Tab.Screen name='Home' component={Home} options={
+            {
+                headerShown: false,
+                tabBarIcon: ({color, size, focused}) => {
+                    if(focused){
+                        return <Ionicons name='home' size={size} color={color}/>
+                    }
+                    return <Ionicons name='home-outline' size={size} color={color}/>
+                }
+            }
+        }/>
+        <Tab.Screen name='Profile' component={Profile} options={
+            {
+                headerShown: false,
+                tabBarIcon: ({color, size, focused}) => {
+                    if(focused){
+                        return <Ionicons name='person' size={size} color={color}/>
+                    }
+                    return <Ionicons name='person-outline' size={size} color={color}/>
+                }
+            }
+        }/>
+        <Tab.Screen name='Search' component={Search} options={
+            {
+                headerShown: false,
+                tabBarIcon: ({color, size, focused}) => {
+                    if(focused){
+                        return <Ionicons name='search' size={size} color={color}/>
+                    }
+                    return <Ionicons name='search-outline' size={size} color={color}/>
+                }
+            }
+        }/>
+        <Tab.Screen name='Favorites' component={Favorites} options={
+            {
+                headerShown: false,
+                tabBarIcon: ({color, size, focused}) => {
+                    if(focused){
+                        return <Ionicons name='bookmark' size={size} color={color}/>
+                    }
+                    return <Ionicons name='bookmark-outline' size={size} color={color}/>
+                }
+            }
+        }/>
     </Tab.Navigator>
   )
 }
